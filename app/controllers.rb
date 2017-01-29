393006877eb1baf08e005d10099adad6593a6fbd
Caique Peixoto <caiquepeixoto1@gmail.com>
2017-01-29 05:22:27 -0200

    [Auth/API] Methods for authentication and account creation added to controllers.rb.

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,23 +1,27 @@
 Heimdall::App.controllers  do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  get :status, map: '/status', provides: :json do
+    'Bifrost bridge is up and running!'
+  end
 
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
+  get :authenticate, map: '/auth', provides: :json do
+    if Account.authenticate(params[:email], params[:password])
+      status 200
+      body 'Authentication was successful!'
+    else
+      status 403
+      body 'Authentication error!'
+    end
+  end
 
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
+  post :create_account, map: '/auth/create', provides: :json do
+    if Account.new(params[:account]).save
+      status 200
+      body 'Account was created!'
+    else
+      status 400
+      body 'Problems when creating account.'
+    end
+  end
 
 end
